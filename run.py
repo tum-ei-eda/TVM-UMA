9056c718089a48ac983b71856023c94e49c51fa5
SamiraAFg <123063568+SamiraAFg@users.noreply.github.com>
2023-08-30 12:13:25 +0200

    Update run.py

--- run.py
+++ run.py
@@ -14,69 +14,124 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import tensorflow as tf
+import os
+import pytest
+
+pytest.importorskip("tflite")
+pytest.importorskip("tensorflow")
+
 from tvm.micro.testing.aot_test_utils import AOT_DEFAULT_RUNNER
 import tvm
 from tvm import relay
-from backend import VanillaAcceleratorBackend
-from tvm.relay import transform
+from backend import QVanillaAcceleratorBackend
+from tvm.relay import transform, testing
 from collections import OrderedDict
 import numpy as np
-
+from tvm.relay.backend import Runtime, Executor
 
 from tvm.testing.aot import (
     AOTTestModel as AOTModel,
     AOTTestRunner as AOTRunner,
     generate_ref_data,
     compile_and_run,
+    create_relay_module_and_inputs_from_tflite_file,
 )
 
 
-def create_conv2d(groups=1, runner=AOT_DEFAULT_RUNNER, weight_shape=32):
-    dtype = "float32"
-    ishape = (1, 32, 14, 14)
-    wshape = (32, weight_shape, 3, 3)
-    pass_config = {"tir.usmp.enable": True}
-    runner = AOTRunner(
-        makefile=runner.makefile,
-        prologue=runner.prologue,
-        epilogue=runner.epilogue,
-        includes=runner.includes,
-        parameters=runner.parameters,
-        pass_config=pass_config,
+def generate_tflite_file(tflite_filename):
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    print(x_train.shape)
+    x_train, x_test = x_train.reshape(-1, 28, 28, 1), x_test.reshape(-1, 28, 28, 1)
+    tf_model = tf.keras.models.Sequential(
+        [
+            tf.keras.Input(shape=(28, 28, 1)),
+            tf.keras.layers.Conv2D(4, (1, 1), padding="same", activation="relu"),
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(32, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10),
+        ]
     )
-    data0 = relay.var("data", shape=ishape, dtype=dtype)
-    weight0 = relay.var("weight", shape=wshape, dtype=dtype)
-    out = relay.nn.conv2d(data0, weight0, kernel_size=(3, 3), padding=(1, 1), groups=groups)
-    main_f = relay.Function([data0, weight0], out)
-    mod = tvm.IRModule()
-    mod["main"] = main_f
-    mod = transform.InferType()(mod)
-    i_data = np.random.uniform(0, 1, ishape).astype(dtype)
-    w1_data = np.random.uniform(0, 1, wshape).astype(dtype)
-    inputs = OrderedDict([("data", i_data), ("weight", w1_data)])
-    output_list = generate_ref_data(mod, inputs)
-    return mod, inputs, output_list, runner
+    output = tf_model(x_train[:1])
+    output = output.numpy()
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    loss(y_train[:1], output).numpy()
+    tf_model.compile(metrics=["accuracy"], optimizer="adam", loss=loss)
+    tf_model.fit(x_train, y_train, epochs=1)
+
+    def representative_dataset():
+        for _ in range(100):
+          data = np.random.rand(1, 28, 28, 1)
+          yield [data.astype(np.float32)]
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(tf_model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.representative_dataset = representative_dataset
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.inference_input_type = tf.int8  # or tf.uint8
+    converter.inference_output_type = tf.int8  # or tf.uint8
+    tflite_quant_model = converter.convert()
+    with open(tflite_filename, "wb") as f:
+        f.write(tflite_quant_model)
+
 
 
 def main():
-    mod, inputs, output_list, runner = create_conv2d()
 
-    uma_backend = VanillaAcceleratorBackend()
+    tflite_file = "/tmp/model.tflite"
+
+    if os.path.exists(tflite_file):
+        os.remove(tflite_file)
+    generate_tflite_file(tflite_file)
+    pytest.importorskip("tflite")
+
+    interpreter = tf.lite.Interpreter(model_path=tflite_file)
+    tf_model_details = interpreter.get_input_details()
+    mod, _, params = create_relay_module_and_inputs_from_tflite_file(
+        tflite_file, bind_params_by_name=False
+    )
+
+    uma_backend = QVanillaAcceleratorBackend()
     uma_backend.register()
-    mod = uma_backend.partition(mod)
-    target = tvm.target.Target("vanilla_accelerator", host=tvm.target.Target("c"))
+    print("registering done")
+    target = tvm.target.Target("q_vanilla_accelerator", host=tvm.target.Target("c"))
+    target_c = tvm.target.Target("c")
+    
+    # Generation of test input and output
+    data_shape = [int(x) for x in mod["main"].params[0].type_annotation.shape]
+    data = np.random.uniform(size=data_shape).astype("int8")
+    input_list = {str(tf_model_details[0]["name"]): data}
+    output_list = generate_ref_data(mod, input_list, params)
 
+    print("before partitioning")
+    mod = uma_backend.partition(mod)
+      
+ 
+    
     export_directory = tvm.contrib.utils.tempdir(keep_for_debug=True).path
     print(f"Generated files are in {export_directory}")
+
+    aot_test_model = AOTModel(module=mod, inputs=input_list, outputs=output_list, params=params)
+
+    test_runner = AOT_DEFAULT_RUNNER
+
     compile_and_run(
-        AOTModel(module=mod, inputs=inputs, outputs=output_list),
-        runner,
+        aot_test_model,
+        test_runner,
         interface_api="c",
         use_unpacked_api=True,
-        target=target,
+        workspace_byte_alignment=1,
+        debug_calculated_workspaces=False,
+        target=[target_c, target],
         test_dir=str(export_directory),
     )
 
 
+    print("compiling")
+
+    
 if __name__ == "__main__":
     main()

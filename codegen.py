1292176fdbc5d92979a7129beee3a59a834c3d81
SamiraAFg <123063568+SamiraAFg@users.noreply.github.com>
2023-08-30 12:05:13 +0200

    Update codegen.py

--- codegen.py
+++ codegen.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""UMA codegen for the vanilla_accelerator accelerator"""
+"""UMA codegen for the q_vanilla_accelerator accelerator"""
 
 import tvm
 import pathlib
